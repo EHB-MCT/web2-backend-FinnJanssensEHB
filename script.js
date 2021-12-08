1453633ec5734e83ab6ed1544fcc07329da8beab
FinnJanssensEHB <finn.janssens@student.ehb.be>
2021-12-08 15:50:40 +0100

    Add cors to express app

--- script.js
+++ script.js
@@ -1,28 +1,34 @@
-import 'dotenv/config';
-import express from 'express';
-import * as mongo from './mongo.js'
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+
+import * as mongo from "./mongo.js";
 
 const app = express();
 
-app.get('/videos', async (req, res) => {
+app.use(cors());
+app.use(express.static("public"));
+app.use(express.json());
+
+app.get("/videos", async (req, res) => {
   await mongo.connectToClient();
-  const videos = await mongo.getCollection('Videos');
+  const videos = await mongo.getCollection("Videos");
   mongo.closeClient();
   return res.send(videos);
 });
 
-app.post('/', (req, res) => {
-  return res.send('Received a POST HTTP method');
+app.post("/", (req, res) => {
+  return res.send("Received a POST HTTP method");
 });
 
-app.put('/', (req, res) => {
-  return res.send('Received a PUT HTTP method');
+app.put("/", (req, res) => {
+  return res.send("Received a PUT HTTP method");
 });
 
-app.delete('/', (req, res) => {
-  return res.send('Received a DELETE HTTP method');
+app.delete("/", (req, res) => {
+  return res.send("Received a DELETE HTTP method");
 });
 
 app.listen(process.env.PORT, () =>
-  console.log(`Example app listening on port ${process.env.PORT}!`),
+  console.log(`Example app listening on port ${process.env.PORT}!`)
 );
