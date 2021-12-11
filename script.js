e930db0046212ea3f90ff51757f3b5a16c6beecc
FinnJanssensEHB <finn.janssens@student.ehb.be>
2021-12-11 11:05:53 +0100

    Add Auth0 authentication

--- script.js
+++ script.js
@@ -1,31 +1,60 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import pkg from "express-openid-connect";
+import path from "path";
+
+const { auth, requiresAuth } = pkg;
 
 import * as mongo from "./mongo.js";
 import * as v from "./vimeo.js";
 
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.AUTH0_CLIENT_SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.AUTH0_CLIENT_ID,
+  issuerBaseURL: process.env.AUTH0_DOMAIN,
+};
+
 const app = express();
 
 app.use(cors());
-// app.use(express.static("public"));
+app.use(express.static("public"));
 app.use(express.json());
+app.use(auth(config));
+
+app.get("/", (req, res) => {
+  if (!req.oidc.isAuthenticated()) {
+    res.redirect("/login");
+  } else {
+    res.redirect("/dashboard");
+  }
+});
+
+app.get("/dashboard", requiresAuth(), (req, res) => {
+  var options = {
+    root: path.join(path.resolve()),
+  };
+
+  var fileName = "index.html";
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      next(err);
+    } else {
+      console.log("Sent:", fileName);
+    }
+  });
+});
+
+app.get("/profile", requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
 
 app.get("/videos", async (req, res) => {
   let videos = await v.GetVideos();
   return res.json(videos);
-});
-
-app.post("/", (req, res) => {
-  return res.send("Received a POST HTTP method");
-});
-
-app.put("/", (req, res) => {
-  return res.send("Received a PUT HTTP method");
-});
-
-app.delete("/", (req, res) => {
-  return res.send("Received a DELETE HTTP method");
 });
 
 app.listen(process.env.PORT, () =>
