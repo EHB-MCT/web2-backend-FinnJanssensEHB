7b1d867af11732f7cba83524bc32bae1d9cf3124
FinnJanssensEHB <finn.janssens@student.ehb.be>
2021-12-16 10:45:13 +0100

    Remove auth0 since it doesn't have a free plan

--- script.js
+++ script.js
@@ -1,44 +1,23 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
-import pkg from "express-openid-connect";
 import path from "path";
-
-const { auth, requiresAuth } = pkg;
 
 import * as mongo from "./mongo.js";
 import * as v from "./vimeo.js";
-
-const config = {
-  authRequired: false,
-  auth0Logout: true,
-  secret: process.env.AUTH0_CLIENT_SECRET,
-  baseURL: process.env.BASE_URL,
-  clientID: process.env.AUTH0_CLIENT_ID,
-  issuerBaseURL: process.env.AUTH0_DOMAIN,
-};
 
 const app = express();
 
 app.use(cors());
 app.use(express.static("public"));
 app.use(express.json());
-app.use(auth(config));
 
 app.get("/", (req, res) => {
-  if (!req.oidc.isAuthenticated()) {
-    res.redirect("/login");
-  } else {
-    res.redirect("/admin");
-  }
+  res.redirect("/admin");
 });
 
-app.use("/admin", requiresAuth());
+// app.use("/admin", requiresAuth());
 app.use("/admin", express.static(path.join(path.resolve(), "admin")));
-
-app.get("/profile", requiresAuth(), (req, res) => {
-  res.send(JSON.stringify(req.oidc.user));
-});
 
 app.get("/videos", async (req, res) => {
   let videos = await v.GetVideos();
